bddb4752ad1918a105135b7f56619070cb7bdbdf
Asif Mahmud <asif01050105@gmail.com>
2021-09-01 23:17:58 +0600

    #2 tweet schema

---
+++ mern/server/model/tweet.model.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+const TweetSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    trim: true,
+    required: 'text is required'
+  },
+  
+  retweets:[ {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tweet',
+  }],
+
+  likes:[ {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+	}],
+
+  count:{
+      type:Number,
+      default:0
+  } ,
+  
+  userID:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+},
+
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  },    
+
+})
+
+
+ 
+
+
+
+
+
+export default mongoose.model('Tweet', TweetSchema)
